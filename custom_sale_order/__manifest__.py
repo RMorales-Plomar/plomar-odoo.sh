9aeffd03bcc3846a4a50955a78f6da02b72314b7
dmunozdelrio <dmunozdelrio@gmail.com>
2023-08-15 15:21:34 -0600

    feature: fixed field position

---
+++ custom_sale_order/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*- 
+
+{
+    'name': 'Custom Sale order',
+    'version': '1.1.5',
+    'license': "AGPL-3",
+    'depends': [
+        'sale_management'
+    ],
+    'data': [
+        'views/sale_order_form.xml'
+    ],
+    'installable': True,
+    'auto_install': False,
+}
