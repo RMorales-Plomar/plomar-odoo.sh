c504f9e2ee6100f2f18b5613320b5d6873d7b664
dmunozdelrio <dmunozdelrio@gmail.com>
2023-08-17 12:36:00 -0600

    campos en form y tree

---
+++ custom_account_move/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*- 
+
+{
+    'name': 'Custom account move',
+    'version': '1.1.5',
+    'license': "AGPL-3",
+    'depends': [
+        'account_accountant',
+        'account'
+    ],
+    'data': [
+        'views/account_move_form.xml'
+    ],
+    'installable': True,
+    'auto_install': False,
+}
