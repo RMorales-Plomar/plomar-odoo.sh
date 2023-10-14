39b8ff6ba030858bb251f9ba3dc589ca66cc102d
Diego Alejandro Muñoz del rio <50757943+dmunoz2019@users.noreply.github.com>
2023-10-14 11:05:40 -0600

    added fel

---
+++ l10n_gt_fe/models/partner.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+import logging
+
+from odoo import _, api, fields, models
+
+_logger = logging.getLogger(__name__)
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    country_id = fields.Many2one(default=lambda s: s.env.ref('base.gt'))
+    partner_type = fields.Selection([
+        ('nit', 'NIT'),
+        ('cui', 'CUI')], string="Tipo de Documento", required=False, default='nit')
+
+    
+
+    
